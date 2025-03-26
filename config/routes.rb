1eae8818e2b4585177d6414ee5f79f88f6e389db
brandong97 <116442100+brandong97@users.noreply.github.com>
2025-03-26 04:13:29 +0000

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get "content_pages/show"
-=======
->>>>>>> 923620a (linking lesson links to lesson pages)
   get "lessons/index"
   get "lessons/show"
   # Root route (default to login page)
