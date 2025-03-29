c40fd28474e61679fe9f07ed4d01e88c49c2e678
Arturo Montemayor <montea5195@gmail.com>
2025-03-29 18:51:11 +0000

    login_controller.rb modifications/ (app/views/login/index.html.erb) created

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,6 +2,19 @@ class LoginController < ApplicationController
   def index
   end
 
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to dashboard_path, notice: "Logged in successfully"
+    else
+      flash[:alert] = "Invalid email or password"
+      render :index
+    end
+  end
+
+
   def destroy
     reset_session # Logs out the user by clearing the session
     redirect_to login_path, notice: "Logged out successfully."
