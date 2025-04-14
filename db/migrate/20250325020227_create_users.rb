2eb242d374531bd940be9c2152b32ef2c22d0c14
Elias Argenal <143857813+EliasArgenal@users.noreply.github.com>
2025-04-14 07:35:59 +0000

    update users migration to use password_digest

--- db/migrate/20250325020227_create_users.rb
+++ db/migrate/20250325020227_create_users.rb
@@ -2,9 +2,10 @@ class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
       t.string :email
-      t.string :user_password
-
+      t.string :password_digest  # Change from user_password to password_digest
       t.timestamps
     end
+
+    add_index :users, :email, unique: true  # Optional but recommended
   end
 end
