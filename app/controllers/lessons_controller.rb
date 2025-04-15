7f5bf9b5f2198250e2d786a1c94a0734932b4e8e
Arturo Montemayor <montea5195@gmail.com>
2025-04-15 02:12:29 +0000

    Updated lessons_controller.rb and  apps/views/lessons/index.html.erb to have lessons with content.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -11,4 +11,13 @@ class LessonsController < ApplicationController
   def index
     @lessons = Lesson.all
   end
+
+  Lesson.create!(
+  [
+    { title: "Lesson 1: Introduction to C++", content: "Learn about syntax, comments, operators, and the main function." },
+    { title: "Lesson 2: Variables in C++", content: "Explore how variables are declared, types used, and naming best practices." },
+    { title: "Lesson 3: Expressions in C++", content: "Understand different kinds of expressions and how they're evaluated." }
+  ]
+)
+
 end
