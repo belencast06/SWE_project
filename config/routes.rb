da9945baf37385f8bed74911639841e05b6f74ac
Elias Argenal <143857813+EliasArgenal@users.noreply.github.com>
2025-04-16 02:34:10 +0000

    removed admin files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get '/admin/cleanup_lessons', to: 'admin#cleanup_lessons'
-  get '/admin/seed_database', to: 'admin#seed_database'
 
   # Home Page
   get '/home', to: 'home#index', as: 'home'
