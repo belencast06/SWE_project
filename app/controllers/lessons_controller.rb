7f36063c71238f65215094c1f19ca9bad855af16
Elias Argenal <143857813+EliasArgenal@users.noreply.github.com>
2025-04-16 01:41:41 +0000

    removed duplicate lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,23 +1,9 @@
 class LessonsController < ApplicationController
-  LESSONS = {
-    "Lesson 1" => "This is Lesson 1 content.",
-    "Lesson 2" => "This is Lesson 2 content.",
-    "Lesson 3" => "This is Lesson 3 content.",
-  }
-
   def show
-    @lesson = Lesson.find(params[:id])  # This fetches a lesson by its ID
+    @lesson = Lesson.find(params[:id])
   end
+  
   def index
     @lessons = Lesson.all
   end
-
-  Lesson.create!(
-  [
-    { title: "Lesson 1: Introduction to C++", content: "Learn about syntax, comments, operators, and the main function." },
-    { title: "Lesson 2: Variables in C++", content: "Explore how variables are declared, types used, and naming best practices." },
-    { title: "Lesson 3: Expressions in C++", content: "Understand different kinds of expressions and how they're evaluated." }
-  ]
-)
-
 end
